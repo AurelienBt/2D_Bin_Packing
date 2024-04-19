9dd4ddffccc7fbe83fbe04b8f377275d1308ebf1
Aurélien BOISSOT <aurelien.boissot@gmail.com>
2024-04-19 11:54:47 +0200

    bin.h + rect.h

---
+++ rect.h
@@ -0,0 +1,38 @@
+#ifndef RECT_H
+#define RECT_H
+
+#include <vector>
+
+class Rect {
+public:
+
+	Rect();
+	~Rect();
+
+	int getHeight();
+	int getWidth();
+	bool getRotation();
+	void setRotation();
+
+	Bin getBin();
+	void setBin(Bin bin);
+
+	int getX();
+	int getY();
+	void setX(int x);
+	void setY(int y);
+
+
+private:
+
+	int height;
+	int width;
+	bool rotation;
+	Bin bin;
+
+	int x;
+	int y;
+
+};
+
+#endif
