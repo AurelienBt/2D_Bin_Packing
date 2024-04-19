5ef8595b47588408794a4faa5002965cf938937e
Aurélien BOISSOT <aurelien.boissot@gmail.com>
2024-04-19 08:33:27 +0200

    Ajoutez des fichiers projet.

---
+++ 2D_Bin_Packing.cpp
@@ -0,0 +1,12 @@
+﻿// 2D_Bin_Packing.cpp : définit le point d'entrée de l'application.
+//
+
+#include "2D_Bin_Packing.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
