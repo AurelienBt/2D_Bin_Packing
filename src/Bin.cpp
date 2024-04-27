8a0a525432b30d4170672e6ce05a3ed75fbb4cfe
Aurélien BOISSOT <aurelien.boissot@gmail.com>
2024-04-27 11:45:59 +0200

    Rect.cpp + Bin.cpp

--- src/Bin.cpp
+++ src/Bin.cpp
@@ -1,1 +1,41 @@
 #include "Bin.h"
+
+Bin::Bin() 
+{
+	this->emptySpace = 0;
+}
+
+Bin::~Bin() 
+{
+
+}
+
+int Bin::getEmptySpace() const
+{
+	//TODO
+	return 0;
+}
+
+void Bin::addRectangle(Rect rect)
+{
+	//TODO ajouter vérif
+	this->rectInBinList.push_back(rect);
+}
+
+void Bin::removeRectangle(Rect rect)
+{
+	//TODO
+	return;
+}
+
+std::vector<Rect> Bin::getRectInBinList() const
+{
+	return this->rectInBinList;
+}
+
+void Bin::updateEmptySpace(Rect addedRect, bool add)
+{
+	//TODO
+	return;
+}
+
--- src/Rect.cpp
+++ src/Rect.cpp
@@ -1,1 +1,78 @@
+#include <iostream>
+
 #include "Rect.h"
+
+Rect::Rect(int id, int height, int width): id(id), height(height), width(width)
+{
+	this->rotation = false;
+	this->x = -1;
+	this->y = -1;
+	this->binId = -1;
+}
+
+Rect::~Rect()
+{
+}
+
+int Rect::getId() const
+{
+	return id;
+}
+
+int Rect::getHeight() const
+{
+	return height;
+}
+
+int Rect::getWidth() const
+{
+	return width;
+}
+
+bool Rect::getRotation() const
+{
+	return rotation;
+}
+
+void Rect::setRotation(bool rotation)
+{
+	this->rotation = rotation;
+}
+
+int Rect::getBinId() const
+{
+	return this->binId;
+}
+
+void Rect::setBinId(int binId)
+{
+	this->binId = binId;
+}
+
+int Rect::getX() const
+{
+	return this->x;
+}
+
+int Rect::getY() const
+{
+	return this->y;
+}
+
+void Rect::setX(int x)
+{
+	this->x = x;
+}
+
+void Rect::setY(int y)
+{
+	this->y = y;
+}
+
+std::ostream& operator<<(std::ostream& os, const Rect & r)
+{
+	os << "ID : " << r.getId() << ", Bin ID : " << r.getBinId() << "\n";
+	os << "Height : " << r.getHeight() << ", Width : " << r.getWidth() << "\n";
+	os << "X : " << r.getX() << ", Y : " << r.getY() << ", Rotation : " << r.getRotation();
+	return os;
+}
